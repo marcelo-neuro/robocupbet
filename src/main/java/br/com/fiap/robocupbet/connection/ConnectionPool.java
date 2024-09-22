7751cdba7cef1b15fb4094113adeec1783fc6f01
Marcelo Furlanetto <marcelofurla0813@gmail.com>
2024-09-22 17:56:08 -0300

    Created connection to OracleDB
    Created the connection pool class, with HikariCP and Oracle JDBC.

---
+++ src/main/java/br/com/fiap/robocupbet/connection/ConnectionPool.java
@@ -0,0 +1,38 @@
+package br.com.fiap.robocupbet.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class ConnectionPool {
+	 private static final String ORACLE_URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL",
+			 	ORACLE_USER = "", //coloque o usuario
+			 	ORACLE_PASSWORD = ""; //coloque o password
+	 
+	 private static HikariDataSource dataSource;
+	 
+	 static {
+		 HikariConfig config = new HikariConfig();
+		 config.setJdbcUrl(ORACLE_URL);
+		 config.setUsername(ORACLE_USER);
+		 config.setPassword(ORACLE_PASSWORD);
+		 
+		 config.setMaximumPoolSize(10);
+		 config.setMinimumIdle(5);
+		 config.setIdleTimeout(60000);
+		 config.setConnectionTimeout(30000);
+		 
+		 dataSource = new HikariDataSource(config);
+	 }
+	 
+	 public static Connection getConnection() {
+		 try {
+			 return dataSource.getConnection();
+		 } catch(SQLException e) {
+			 e.printStackTrace();
+		 }
+		 return null;
+	 }
+}
