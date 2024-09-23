e24a04092df0029fffae958001f9b0da32d25eec
Marcelo Furlanetto <marcelofurla0813@gmail.com>
2024-09-23 18:07:11 -0300

    resto

---
+++ src/main/java/br/com/fiap/robocupbet/dao/UsuarioDAO.java
@@ -0,0 +1,14 @@
+package br.com.fiap.robocupbet.dao;
+
+import java.sql.Connection;
+
+public class UsuarioDAO {
+
+	private Connection con;
+	
+	public UsuarioDAO(Connection con) {
+		this.con = con;
+	}
+	
+	
+}
