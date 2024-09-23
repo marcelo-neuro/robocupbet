d911c36175ea317b3624570d94884d244ea70fca
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-23 11:41:55 -0300

    commit tela de lutas um pouco mais bonita

--- src/main/java/br/com/fiap/robocupbet/connection/ConnectionPool.java
+++ src/main/java/br/com/fiap/robocupbet/connection/ConnectionPool.java
@@ -1,7 +1,6 @@
 package br.com.fiap.robocupbet.connection;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -12,9 +11,25 @@ public class ConnectionPool {
 			 	ORACLE_USER = "rm551107", //coloque o usuario
 			 	ORACLE_PASSWORD = "210205"; //coloque o password
 	 
+	 private static HikariDataSource dataSource;
+	 
+	 static {
+		 HikariConfig config = new HikariConfig();
+		 config.setJdbcUrl(ORACLE_URL);
+		 config.setUsername(ORACLE_USER);
+		 config.setPassword(ORACLE_PASSWORD);
+		 
+		 config.setMaximumPoolSize(10);
+		 config.setMinimumIdle(5);
+		 config.setIdleTimeout(60000);
+		 config.setConnectionTimeout(30000);
+		 
+		 dataSource = new HikariDataSource(config);
+	 }
+	 
 	 public static Connection getConnection() {
 		 try {
-			 return DriverManager.getConnection(ORACLE_URL, ORACLE_USER, ORACLE_PASSWORD);
+			 return dataSource.getConnection();
 		 } catch(SQLException e) {
 			 e.printStackTrace();
 		 }
